e956f7b2702b2150ac99287b3055e27c4fffeb00
vikrant <tnarkiv.pradhan@gmail.com>
2018-07-09 09:45:21 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('name', (name) => {
+    return `my name is ${name}`
+})
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    console.log(now, req.method, req.url);
+    next();
+})
+
+app.get('/', (req, res) => {
+    res.send('welcome');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        text1: "yo",
+        text2: "ho",
+        year: 1564
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+});
